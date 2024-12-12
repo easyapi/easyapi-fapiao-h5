2f9d5472af36f99f26635ac60825b429344127b3
chenyunc123 <1537299563@qq.com>
2024-12-12 12:09:28 +0800

    发票复制文案优化

--- src/utils/invoice.ts
+++ src/utils/invoice.ts
@@ -2,10 +2,5 @@
  * 获取复制文案
  */
 export function copyText(invoice: any) {
-  return `${invoice.purchaserName}
-   收到一张发票，${invoice.category} 
-   ${invoice.price}元，开票日期：${invoice.printTime ? invoice.printTime.substring(0, 10) : ''}，
-   ${invoice.code ? `发票代码：${invoice.code}，` : ''}${invoice.number ? `发票号码：${invoice.number}，` : ''}
-   ${invoice.allElectronicInvoiceNumber ? `全电号码：${invoice.allElectronicInvoiceNumber}，` : ''}
-   电子税务局下载页：${invoice.downloadUrl}`
+  return `${invoice.purchaserName}收到一张发票，${invoice.category}${invoice.price}元，开票日期：${invoice.printTime ? invoice.printTime.substring(0, 10) : ''}，${invoice.code ? `发票代码：${invoice.code}，` : ''}${invoice.number ? `发票号码：${invoice.number}，` : ''}${invoice.allElectronicInvoiceNumber ? `全电号码：${invoice.allElectronicInvoiceNumber}，` : ''}${invoice.downloadUrl ? `电子税务局下载页：${invoice.downloadUrl}` : `下载地址：${invoice.electronicInvoiceUrl}`}`
 }
